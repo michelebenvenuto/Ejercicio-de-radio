6a5f7900ef24e6337b5ed97fcd4788df426d5c3d
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2018-07-05 17:41:22 -0600

    Se creo la clase radio y se probo

---
+++ src/Radio.kt
@@ -0,0 +1,28 @@
+package radio
+fun main(arg: Array<String>){class Radio (
+        var encendido: Boolean = false,
+        var frecuencia: String = "FM",
+        var volumen: Int = 0,
+        var estacion: Int
+) {
+    fun encender() {
+        encendido = true
+    }
+
+    fun apagar() {
+        encendido = false
+    }
+
+    override fun toString(): String {
+        return """
+            Radio:
+                Encendido: $encendido
+                Frecuencia: $frecuencia
+                Volumen: $volumen
+                Estacion: $estacion
+        """.trimIndent()
+    }
+}
+    val radio = Radio(estacion= 87)
+    println(radio)
+}
